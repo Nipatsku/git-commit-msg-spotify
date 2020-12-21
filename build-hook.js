d02af1036bc801581c42895d934fc5d5bafa6c8d
niilo <niilo.keinanen@hotmail.com>
2020-12-21 17:39:52 +0200

    Fix hook dest

--- build-hook.js
+++ build-hook.js
@@ -19,21 +19,27 @@ if ( fs.existsSync( outFolder, clbk ) ) {
     fs.rmdirSync( outFolder, {recursive: true} )
 }
 // Create out folder.
+console.log( outFolder )
 fs.mkdir(outFolder, clbk)
 
 // Write .env file
-fs.writeFileSync( `${outFolder}/.env`, `SPOTIFY_ID=${SPOTIFY_ID}
+const envFile = `${outFolder}/.env`
+console.log( envFile )
+fs.writeFileSync( envFile, `SPOTIFY_ID=${SPOTIFY_ID}
 BACKEND_URL=${BACKEND_URL}
 `, 'utf8' )
 
 // Write commit-msg file
-const commitMsgHook = `${outFolder}/.git/hooks/commit-msg`
+const commitMsgHook = `../.git/hooks/commit-msg`
 if ( fs.existsSync( commitMsgHook, clbk ) ) {
     console.warn(`WARNING: ${commitMsgHook} will be replaced. Back-upping previous file to ${commitMsgHook}.bup`)
     fs.copyFileSync( commitMsgHook, commitMsgHook + '.bup' )
 }
 fs.mkdirSync( `${outFolder}/.git/hooks/`, {recursive: true}, clbk )
+console.log( commitMsgHook )
 fs.copyFileSync( `./commit-msg`, commitMsgHook )
 
 // Write commit-msg.js file
-fs.copyFileSync( `./commit-msg.js`, `${outFolder}/commit-msg.js` )
+const commitMsgJsFile = `${outFolder}/commit-msg.js`
+console.log(commitMsgJsFile)
+fs.copyFileSync( `./commit-msg.js`, commitMsgJsFile )
